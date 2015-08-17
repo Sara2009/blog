98ba7858eda9a7ca6815c1ec5ca3592190d04f2e
Sara2009 <727204211@qq.com>
2015-08-17 11:28:30 +0800

    facebox完整版
    添加facebox的样式，还缺少付出右上角的close

--- src/facebox/facebox.js
+++ src/facebox/facebox.js
@@ -1,4 +1,5 @@
-;require(['jquery'], function ($) {
+;define(['jquery'], function ($) {
+	'user strict';
 	var Modal = function (element, options) {
 		var _ = this;
 		_.o = options;
@@ -12,6 +13,9 @@
 		_.$element.on('click', function () {
 			_.render();
 		});
+		_.$modal.find('.btn-cancle').on('click', function () {
+			_.hide();
+		})
 	};
 
 
@@ -29,24 +33,34 @@
             })   
             .prependTo(_.$modal)   
             .fadeIn(100); 
-        _.$modal.find('.modal-dialog').css({
+        /*_.$modal.find('.modal-dialog').css({
         	'position': 'absolute',
         	'left': windowWidth/2-modalWidth/2,
         	'top': 50,
         	'z-index': 1
-        });
+        });*/
+		/*_.$modal.find('.modal-dialog').css({
+        	'position': 'relative',
+        	'margin': '20px auto',
+        });*/
         _.$modal.show();
 	};
 
 	Modal.prototype.hide = function () {
 		var _ = this;
 		$('.modal-backup').remove();
+		_.$modal.find('.modal-dialog').css({
+        	'position': 'relative',
+        	'left': 0,
+        	'top': 0,
+        	'z-index': 0
+        });
 		_.$modal.hide();
 		
 		
 	};
 
-	$.fn.modal = function (options) {
+	$.fn.facebox = function (options) {
         var len = this.length;
         return this.each(function(index) {
             var instance = new Modal(this, options);
